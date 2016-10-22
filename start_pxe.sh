b09e7cc7b7920828ad8d24bb1314ce700550da00
William Zhang <willzhang05@gmail.com>
2016-10-22 22:12:23 +0000

    Revert darkhttpd back to port 80

--- start_pxe.sh
+++ start_pxe.sh
@@ -5,7 +5,7 @@ mount /root/ubuntu/ubuntu-16.04.1-desktop-amd64.iso /mnt/ubuntu/amd64
 mount /root/arch.iso /mnt/arch
 
 ip link set enp0s25 up
-ip addr add 198.168.0.1/24 dev enp0s25
+ip addr add 192.168.0.1/24 dev enp0s25
 
 systemctl start dnsmasq
-darkhttpd /srv/pxe/arch --port 8081
+darkhttpd /srv/pxe/arch
